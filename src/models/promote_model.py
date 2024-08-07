bc7f275e4e23bb3c8884016bc4297b588e9fb94b
Ubaidmalik9567 <ubaidmalik9567@yahoo.com>
2024-08-07 20:32:54 +0500

    add run_name

--- src/models/promote_model.py
+++ src/models/promote_model.py
@@ -21,24 +21,27 @@ def promote_model():
 
     model_name = "save_model"
     # Get the latest version in staging
-    latest_version_staging = client.get_latest_versions(model_name, stages=["Staging"])[0].version
+    
+    with mlflow.start_run(run_name="model_promoted_file-run") as run:
+        # Get the latest version in staging
+        latest_version_staging = client.get_latest_versions(model_name, stages=["Staging"])[0].version
 
-    # Archive the current production model
-    prod_versions = client.get_latest_versions(model_name, stages=["Production"])
-    for version in prod_versions:
+        # Archive the current production model
+        prod_versions = client.get_latest_versions(model_name, stages=["Production"])
+        for version in prod_versions:
+            client.transition_model_version_stage(
+                name=model_name,
+                version=version.version,
+                stage="Archived"
+            )
+
+        # Promote the new model to production
         client.transition_model_version_stage(
             name=model_name,
-            version=version.version,
-            stage="Archived"
+            version=latest_version_staging,
+            stage="Production"
         )
-
-    # Promote the new model to production
-    client.transition_model_version_stage(
-        name=model_name,
-        version=latest_version_staging,
-        stage="Production"
-    )
-    print(f"Model version {latest_version_staging} promoted to Production")
+        print(f"Model version {latest_version_staging} promoted to Production")
 
 if __name__ == "__main__":
     promote_model()
