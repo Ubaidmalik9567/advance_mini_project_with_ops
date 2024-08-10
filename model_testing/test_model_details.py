be55a88ed6f50568b0b411b7e6b4d84a620fdfc7
Ubaidmalik9567 <ubaidmalik9567@yahoo.com>
2024-08-10 15:42:46 +0500

    load latest model

--- model_testing/test_model_details.py
+++ model_testing/test_model_details.py
@@ -32,27 +32,38 @@ class TestModelLoading(unittest.TestCase):
         cls.new_model_uri = f'models:/{cls.new_model_name}/{cls.new_model_version}'
         cls.new_model = mlflow.pyfunc.load_model(cls.new_model_uri)
 
-    
     @staticmethod
-    def get_latest_model_version(model_name, stage="Staging"):
+    def get_latest_model_version(model_name, stage="Production"):
         client = mlflow.MlflowClient()
-        latest_version = client.get_latest_versions(model_name, stages=[stage])
-        return latest_version[0].version if latest_version else None
-    
-    # @staticmethod
-    # def get_latest_model_version(model_name):
-    #     client = mlflow.MlflowClient()
-    #     # Fetch all versions and sort them by version number
-    #     versions = client.get_latest_versions(model_name)
-    #     versions.sort(key=lambda v: int(v.version), reverse=True)
-    #     return versions[0].version if versions else None
+        # First try to get the latest version in the "Production" stage
+        latest_versions = client.get_latest_versions(model_name, stages=[stage])
+        
+        if not latest_versions:
+            # If no versions in "Production", fallback to "None"
+            latest_versions = client.get_latest_versions(model_name, stages=["None"])
+        
+        if latest_versions:
+            latest_version = latest_versions[0]
+            version = latest_version.version
+            run_id = latest_version.run_id  # Get the run_id of the model version
+            model_uri = f"models:/{model_name}/{version}"
+            
+            print(f"Model Name: {model_name}")
+            print(f"Latest Model Version: {version}")
+            print(f"Model URI: {model_uri}")
+            print(f"Run ID: {run_id}")  # Print the run_id
+            
+            return version
+        else:
+            print(f"No versions found for model: {model_name}")
+            return None
 
     def test_model_loaded_properly(self):
         self.assertIsNotNone(self.new_model)
     
 
-    ''' above code is load model: is 1st model_testing stage which load model sucessfully from model resigtery'''
-    ''' above code is model signature: is 2nd model_testing stage which tell expected input which us require or give same expected output which we want'''
+    ''' above code is load model: is 1st model_testing stage which load model successfully from model registry'''
+    ''' above code is model signature: is 2nd model_testing stage which tells expected input which us require or give same expected output which we want'''
 
 
 if __name__ == "__main__":
