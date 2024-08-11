36493eb9366cf06481cdda885ccc246575860870
Ubaidmalik9567 <ubaidmalik9567@yahoo.com>
2024-08-11 19:39:05 +0500

    change

--- model_testing/test_model_details.py
+++ model_testing/test_model_details.py
@@ -1,11 +1,10 @@
-# that perform by tester, we just for practicing
-
 import unittest
 import mlflow
 import os
-import pandas as pd
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import pickle
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class TestModelLoading(unittest.TestCase):
 
@@ -26,34 +25,29 @@ class TestModelLoading(unittest.TestCase):
         # Set up MLflow tracking URI
         mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
-        # Load the new model from MLflow model registry
+        # Set the model name and stage
         cls.new_model_name = "save_model"
-        cls.new_model_version = cls.get_latest_model_version(cls.new_model_name)
+        cls.stage = "Production"  # Change stage if needed
+
+        # Load the model from MLflow model registry
+        cls.new_model_version = cls.get_latest_model_version(cls.new_model_name, cls.stage)
+        if not cls.new_model_version:
+            raise Exception(f"No model found in the '{cls.stage}' stage.")
+        
         cls.new_model_uri = f'models:/{cls.new_model_name}/{cls.new_model_version}'
         cls.new_model = mlflow.pyfunc.load_model(cls.new_model_uri)
 
-    
     @staticmethod
-    def get_latest_model_version(model_name, stage="Staging"):
+    def get_latest_model_version(model_name, stage="Production"):
         client = mlflow.MlflowClient()
-        latest_version = client.get_latest_versions(model_name, stages=[stage])
-        return latest_version[0].version if latest_version else None
-    
-    # @staticmethod
-    # def get_latest_model_version(model_name):
-    #     client = mlflow.MlflowClient()
-    #     # Fetch all versions and sort them by version number
-    #     versions = client.get_latest_versions(model_name)
-    #     versions.sort(key=lambda v: int(v.version), reverse=True)
-    #     return versions[0].version if versions else None
+        model_versions = client.search_model_versions(f"name='{model_name}'")
+        latest_version_info = next(
+            (v for v in model_versions if v.current_stage == stage), None
+        )
+        return latest_version_info.version if latest_version_info else None
 
     def test_model_loaded_properly(self):
         self.assertIsNotNone(self.new_model)
-    
-
-    ''' above code is load model: is 1st model_testing stage which load model sucessfully from model resigtery'''
-    ''' above code is model signature: is 2nd model_testing stage which tell expected input which us require or give same expected output which we want'''
-
 
 if __name__ == "__main__":
     unittest.main()
