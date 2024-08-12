95ef30ebb54d97e5132a7e082fa4e461d54b0dc1
Ubaidmalik9567 <ubaidmalik9567@yahoo.com>
2024-08-12 14:42:44 +0500

    change metrix

--- model_testing/test_model_details.py
+++ model_testing/test_model_details.py
@@ -137,7 +137,7 @@ class TestModelLoading(unittest.TestCase):
 
         # Define expected thresholds for the performance metrics
         expected_accuracy = 0.77
-        expected_precision = 0.77
+        expected_precision = 0.80
         expected_recall = 0.77
         expected_f1 = 0.77
 
