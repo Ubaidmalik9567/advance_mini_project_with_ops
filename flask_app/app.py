b10fe33c228dc67e7f3bf5a7d0abd35e9855996d
Ubaidmalik9567 <ubaidmalik9567@yahoo.com>
2024-08-15 16:46:13 +0500

    check new code

--- flask_app/app.py
+++ flask_app/app.py
@@ -8,7 +8,6 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import logging
-import dagshub
 import nltk
 
 # nltk.download('stopwords')
@@ -49,9 +48,20 @@ def normalize_text(text):
     text = lemmatization(text)
     return text
 
-# Set up DagsHub and MLflow
-dagshub.init(repo_owner='Ubaidmalik9567', repo_name='mini_project_with_ops', mlflow=True)
-mlflow.set_tracking_uri("https://dagshub.com/Ubaidmalik9567/mini_project_with_ops.mlflow")
+# Set up DagsHub credentials for MLflow tracking by using key-based authentication
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "Ubaidmalik9567"
+repo_name = "mini_project_with_ops"
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 app = Flask(__name__)
 
