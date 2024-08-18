67f2f728b9f2b65297a71de2ae5f6b38bc9995d5
Ubaidmalik9567 <ubaidmalik9567@yahoo.com>
2024-08-18 20:26:32 +0500

    run it

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import logging
 import os
-import dagshub
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -45,10 +44,6 @@ def normalize_text(text):
     text = removing_urls(text)
     text = lemmatization(text)
     return text
-
-# Initialize Dagshub
-dagshub.init(repo_owner='Ubaidmalik9567', repo_name='mini_project_with_ops', mlflow=True)
-mlflow.set_tracking_uri("https://dagshub.com/Ubaidmalik9567/mini_project_with_ops.mlflow")
 
 app = Flask(__name__)
 
