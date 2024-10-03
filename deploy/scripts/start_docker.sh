40ece80b2989584ed68cac8eaf1e9bea4b402f46
Ubaidmalik9567 <ubaidmalik9567@yahoo.com>
2024-10-03 11:42:23 +0500

    [200~Added AppSpec file with lifecycle hooks for CodeDeploy

---
+++ deploy/scripts/start_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Login AWS ECR
+
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 730335254649.dkr.ecr.eu-north-1.amazonaws.com
+docker pull 730335254649.dkr.ecr.eu-north-1.amazonaws.com/docker_image_ecr2ec2:latest
+docker stop my-app || true
+docker rm my-app || true
+docker run -d -p 8000:8000 -e DAGSHUB_PAT=c6ea9f2beb523e479f6f8150da41c0535c1f50d1 --name my-app 730335254649.dkr.ecr.eu-north-1.amazonaws.com/docker_image_ecr2ec2:latest
